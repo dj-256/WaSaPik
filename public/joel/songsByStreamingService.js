588576b72844a157d66970fad79c0642b591a424
Joël DIBASSO <51450344+dj-256@users.noreply.github.com>
2023-11-01 12:09:00 +0100

    Streaming services (#1)
    * Add streaming services bar chart visualization
    * Create new joel directory with index.html and script.js
    * Add all necessary images and data
    * Add prettier and eslint to project
    * Add prettier as pre commit hook

---
+++ public/joel/songsByStreamingService.js
@@ -0,0 +1,15 @@
+export default [
+  { name: "Deezer", value: 64965 },
+  { name: "iTunes", value: 37387 },
+  {
+    name: "Amazon",
+    value: 13364,
+  },
+  { name: "Spotify", value: 39239 },
+  { name: "YouTube", value: 6043 },
+  {
+    name: "Hype Machine",
+    value: 390,
+  },
+  { name: "Pandora", value: 390 },
+];
