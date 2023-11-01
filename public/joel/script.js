ce2af210d301ed01e58aadce979f90fa37f5b8a0
Joël DIBASSO <51450344+dj-256@users.noreply.github.com>
2023-11-01 15:25:31 +0100

    Add animations and enhance look of bar chart (#3)

--- public/joel/script.js
+++ public/joel/script.js
@@ -9,54 +9,30 @@ let marginLeft = 50;
 let marginRight = 50;
 let logoPadding = 10;
 
-// function roundToNearest(num) {
-//     const power = Math.round(Math.log10(num));
-//     const magnitude = Math.pow(10, power);
-//     const mantissa = num / magnitude;
-//     const roundedMantissa = Math.round(mantissa);
-//     return roundedMantissa * magnitude;
-// }
-
 const filter = new Set(songsByStreamingService.map((d) => d.name));
-
-const data = songsByStreamingService.filter((d) => filter.has(d.name));
-const xScale = d3
-  .scaleBand()
-  .domain(data.map((d) => d.name))
-  .range([marginLeft, containerWidth - marginRight])
-  .padding(0.2);
-
-const yScale = d3
-  .scaleLinear()
-  .domain([0, 100000])
-  .range([containerHeight - marginBottom, marginTop]);
-
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
-
 const container = d3
   .select("#container")
   .attr("width", containerWidth)
   .attr("height", containerHeight);
 
-container
-  .append("g")
-  .attr("transform", `translate(0, ${containerHeight - marginBottom})`)
-  .call(xAxis);
+let data = [];
 
-container
-  .append("g")
-  .attr("transform", `translate(${marginLeft}, 0)`)
-  .call(yAxis);
+const draw = () => {
+  data = songsByStreamingService.filter((d) => filter.has(d.name));
+  console.log(data);
 
-container
-  .selectAll(".bar")
-  .data(data.map((d) => d.value))
-  .enter()
-  .append("path")
-  .attr("fill", "url(#gradient)")
-  .attr("class", "bar")
-  .attr("d", (d, i) => {
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.name))
+    .range([marginLeft, containerWidth - marginRight])
+    .padding(0.2);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 100000])
+    .range([containerHeight - marginBottom, marginTop]);
+
+  const drawBar = (d, i) => {
     // thx copilot
     let x = xScale(data[i].name);
     let y = yScale(d) - marginBottom;
@@ -64,50 +40,176 @@ container
     let height = containerHeight - yScale(d);
     let radius = 10;
     return `M ${x} ${y}
-                    h ${width - radius}
-                    a ${radius} ${radius} 0 0 1 ${radius} ${radius}
-                    v ${height - radius}
-                    h ${-width}
-                    v ${-height + radius}
-                    a ${radius} ${radius} 0 0 1 ${radius} ${-radius}`;
-  });
+            h ${width - radius}
+            a ${radius} ${radius} 0 0 1 ${radius} ${radius}
+            v ${height - radius}
+            h ${-width}
+            v ${-height + radius}
+            a ${radius} ${radius} 0 0 1 ${radius} ${-radius}`;
+  };
+
+  const drawBarSmall = (d, i) => {
+    // thx copilot
+    let x = xScale(data[i].name);
+    let width = xScale.bandwidth();
+    let radius = 10;
+    return `M ${x} ${containerHeight - marginBottom - 10}
+            h ${width - radius}
+            a ${radius} ${radius} 0 0 1 ${radius} ${radius}
+            v ${10 - radius}
+            h ${-width}
+            v ${-10 + radius}
+            a ${radius} ${radius} 0 0 1 ${radius} ${-radius}`;
+  };
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  const t = d3.transition().duration(750);
+
+  container
+    .select(".x-axis")
+    .transition(d3.transition().duration(750))
+    .call(xAxis);
+
+  container.select(".y-axis").call(yAxis);
+
+  container
+    .selectAll(".bar")
+    .data(
+      data.map((d) => d.value),
+      (d) => d.name,
+    )
+    .join(
+      (enter) =>
+        enter
+          .append("path")
+          .attr("fill", "url(#gradient)")
+          .attr("class", "bar")
+          .attr("d", (d, i) => drawBarSmall(d, i))
+          .call((enter) =>
+            enter.transition(t).attr("d", (d, i) => drawBar(d, i)),
+          ),
+      (update) =>
+        update.call((update) =>
+          update.transition(t).attr("d", (d, i) => drawBar(d, i)),
+        ),
+      (exit) =>
+        exit.call((exit) =>
+          exit
+            .transition(d3.transition().duration(250))
+            .attr("fill", "#FFFFFF")
+            .remove(),
+        ),
+    );
+
+  container
+    .selectAll("image")
+    .data(data, (d) => d.name)
+    .join(
+      (enter) =>
+        enter
+          .append("image")
+          .attr("xlink:href", (d) => "../data/images/logo_" + d.name + ".png")
+          .attr("width", 0.5 * xScale.bandwidth())
+          .attr("height", 0.5 * xScale.bandwidth())
+          .attr("class", (d) => "logo " + d.name.toLowerCase())
+          .attr("x", (d) => xScale(d.name) + 0.25 * xScale.bandwidth())
+          .attr(
+            "y",
+            containerHeight -
+              10 -
+              marginBottom -
+              logoPadding -
+              0.5 * xScale.bandwidth(),
+          )
+          .call((enter) =>
+            enter
+              .transition(t)
+              .attr(
+                "y",
+                (d) =>
+                  yScale(d.value) -
+                  marginBottom -
+                  logoPadding -
+                  0.5 * xScale.bandwidth(),
+              ),
+          ),
+      (update) =>
+        update.call((update) =>
+          update
+            .transition(t)
+            .attr("width", 0.5 * xScale.bandwidth())
+            .attr("height", 0.5 * xScale.bandwidth())
+            .attr("x", (d) => xScale(d.name) + 0.25 * xScale.bandwidth())
+            .attr(
+              "y",
+              (d) =>
+                yScale(d.value) -
+                marginBottom -
+                logoPadding -
+                0.5 * xScale.bandwidth(),
+            ),
+        ),
+      (exit) =>
+        exit.call((exit) =>
+          exit
+            .transition(d3.transition().duration(200))
+            .style("opacity", 0)
+            .remove(),
+        ),
+    );
+
+  d3.select(".filterButtons")
+    .selectAll(".filterButton")
+    .data(songsByStreamingService, (d) => d.name)
+    .join(
+      (enter) =>
+        enter
+          .append("span")
+          .attr(
+            "class",
+            (d) =>
+              "mx-2 py-2 px-3 badge pill bg-primary filterButton" +
+              (filter.has(d.name) ? " active" : " inactive"),
+          )
+          .style("cursor", "pointer")
+          .style("border-radius", "50em")
+          .style("padding", "0.5em 1em")
+          .text((d) => d.name)
+          .on("click", function (e, d) {
+            console.log(d.name);
+            if (filter.has(d.name)) {
+              filter.delete(d.name);
+            } else {
+              filter.add(d.name);
+            }
+            draw();
+          }),
+      (update) =>
+        update.attr(
+          "class",
+          (d) =>
+            "mx-2 py-2 px-3 badge pill bg-primary filterButton" +
+            (filter.has(d.name) ? " active" : " inactive"),
+        ),
+    );
+};
 
 container
   .append("g")
-  .attr("class", "logos")
-  .selectAll("image")
-  .data(data)
-  .enter()
-  .append("image")
-  .attr("xlink:href", (d) => "../data/images/logo_" + d.name + ".png")
-  .attr("width", 0.5 * xScale.bandwidth())
-  .attr("height", 0.5 * xScale.bandwidth())
-  .attr("x", (d, i) => xScale(data[i].name) + 0.25 * xScale.bandwidth())
-  .attr(
-    "y",
-    (d) =>
-      yScale(d.value) - marginBottom - logoPadding - 0.5 * xScale.bandwidth(),
-  );
+  .attr("class", "x-axis")
+  .attr("transform", `translate(0, ${containerHeight - marginBottom})`);
 
-const filterButtons = d3
-  .select("#main")
+container
+  .append("g")
+  .attr("class", "y-axis")
+  .attr("transform", `translate(${marginLeft}, 0)`);
+
+d3.select("#main")
   .append("div")
-  .attr("class", "d-flex filterButtons")
-  .selectAll(".filterButton")
-  .data(songsByStreamingService)
-  .enter()
-  .append("span")
-  .attr("class", "mx-2 py-2 px-3 badge rounded-pill bg-primary filterButton")
-  .text((d) => d.name)
-  .on("click", function (e, d) {
-    if (filter.has(d.name)) {
-      filter.delete(d.name);
-    } else {
-      filter.add(d.name);
-    }
-    container.selectAll(".bar").remove();
-    container.selectAll("image").remove();
-    filterButtons.selectAll(".filterButton").remove();
-  })
-  .exit()
-  .remove();
+  .attr("class", "filterButtons")
+  .style("display", "flex")
+  .style("gap", "1em");
+
+draw();
